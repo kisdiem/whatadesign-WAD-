d62fc7947dd2d7e802bba59c8d942dfc7e931919
kisdiem <161992910+kisdiem@users.noreply.github.com>
2026-08-09 05:45:08 +0800

    Apply unified dark console theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider, theme } from 'antd'
 import { BrowserRouter } from 'react-router-dom'
 import 'antd/dist/reset.css'
 import './styles.css'
@@ -7,8 +8,22 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        token: {
+          colorPrimary: '#4da3ff',
+          colorBgBase: '#071019',
+          colorBgContainer: '#0d1824',
+          colorBgElevated: '#101d2a',
+          colorBorder: '#1d3044',
+          borderRadius: 9,
+        },
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
   </React.StrictMode>,
 )
